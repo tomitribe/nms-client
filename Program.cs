ca92b3dd240c83c86f93fcd7505950aad5cab774
Jonathan Gallimore <jon@jrg.me.uk>
2019-02-25 22:59:34 +0000

    Sample producer / consumer code

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using Apache.NMS;
+using Apache.NMS.ActiveMQ;
+using Apache.NMS.Util;
 
 namespace nmsclient
 {
@@ -6,7 +9,49 @@ namespace nmsclient
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Uri connecturi = new Uri("activemq:ssl://gateway.jlpsoftwareltd.net:61616");
+
+            Console.WriteLine("About to connect to " + connecturi);
+
+            // NOTE: ensure the nmsprovider-activemq.config file exists in the executable folder.
+            IConnectionFactory factory = new NMSConnectionFactory(connecturi);
+
+            using (IConnection connection = factory.CreateConnection("robot", "J0hnny5Al1v3"))
+            using (ISession session = connection.CreateSession())
+            {
+                IDestination destination = SessionUtil.GetDestination(session, "queue://FOO.BAR");
+                Console.WriteLine("Using destination: " + destination);
+
+                // Create a consumer and producer
+                using (IMessageConsumer consumer = session.CreateConsumer(destination))
+                using (IMessageProducer producer = session.CreateProducer(destination))
+                {
+                    // Start the connection so that messages will be processed.
+                    connection.Start();
+                    producer.DeliveryMode = MsgDeliveryMode.Persistent;
+
+                    // Send a message
+                    ITextMessage request = session.CreateTextMessage("Hello World!");
+                    request.NMSCorrelationID = "abc";
+                    request.Properties["NMSXGroupID"] = "cheese";
+                    request.Properties["myHeader"] = "Cheddar";
+
+                    producer.Send(request);
+
+                    // Consume a message
+                    ITextMessage message = consumer.Receive() as ITextMessage;
+                    if (message == null)
+                    {
+                        Console.WriteLine("No message received!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Received message with ID:   " + message.NMSMessageId);
+                        Console.WriteLine("Received message with text: " + message.Text);
+                    }
+                }
+            }
+
         }
     }
 }
